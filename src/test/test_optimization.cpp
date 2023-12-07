54e541bb09017e1b058d629f76951fc6bbddf832
hpsleuth7 <murphy.c.austin@gmail.com>
2023-12-07 15:42:00 +0000

    accept command line args for circuit file and ecc file

--- src/test/test_optimization.cpp
+++ src/test/test_optimization.cpp
@@ -3,16 +3,25 @@
 #include "quartz/gate/gate_utils.h"
 
 #include <iostream>
+#include <string>
 
 using namespace quartz;
 
-int main() {
+int main(int argc, char **argv) {
+  
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0] << " <circuit_file.qasm> <ecc_file.json>"
+              << std::endl;
+    return 1;
+  }
+  std::string circuit_filename = argv[1];
+  std::string ecc_filename = argv[2];
+
   Context ctx({GateType::input_qubit, GateType::input_param, GateType::cx,
                GateType::h, GateType::s, GateType::t, GateType::tdg,
                GateType::x, GateType::add, GateType::z});
   //   test_optimization(&ctx, "circuit/example-circuits/voqc_fig5.qasm",
   //                     "cmake-build-debug/bfs_verified.json");
-  test_optimization(&ctx, "circuit/example-circuits/barenco_tof_3.qasm",
-                    "bfs_verified_simplified.json",
+  test_optimization(&ctx, circuit_filename, ecc_filename,
                     false /*use_simulated_annealing*/);
 }
